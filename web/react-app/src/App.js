25bb81edb46d0e3b7fee5a96df8de4dfdd09e040
ldiago <ldiago@gmail.com>
2024-03-04 03:56:39 +0900

    Create App.js

---
+++ web/react-app/src/App.js
@@ -0,0 +1,14 @@
+   // App.js
+   import React from 'react';
+   import Login from './Login';
+
+   function App() {
+     return (
+       <div>
+         <h1>Vite Parse Login</h1>
+         <Login />
+       </div>
+     );
+   }
+
+   export default App;
