41be80bda9abbf06fad983cb3440674a7e5725d5
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2024-03-04 12:24:01 +0100

    Simplify the react application

--- web/react-app/parseConfig.jsx
+++
@@ -1,6 +0,0 @@
-// parseConfig.js
-   import Parse from 'parse';
-
-   // Initialize Parse with your Parse Server information ('yourAppId', 'yourJavascriptKey', 'yourMasterKey')
-   Parse.initialize('appid', 'secretappkey', 'secretappkey');
-   Parse.serverURL = 'https://localhost/api';
