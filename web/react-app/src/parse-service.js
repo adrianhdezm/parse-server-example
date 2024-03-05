ae25ae610efb338a607675ddb381414196f7450d
ldiago <ldiago@gmail.com>
2024-03-05 16:40:02 +0900

    Adding code for Parse logIn

--- web/react-app/src/parse-service.js
+++ web/react-app/src/parse-service.js
@@ -2,7 +2,19 @@ export async function parseLogin(username, password) {
   try {
     // here is the call to Parse server login
     if (username && password) {
-      return true;
+      await Parse.User.logIn(username, password, {
+        success: function(user) {
+         console.log("Logged in: " + user);
+         // user_data = user;
+         // res.render(__dirname + '/public/views/dashboard.html');
+          return true;
+        },
+        error: function(user, error) {
+         console.log("Error: " + error + "\n" + user);
+         // res.send('Incorrect credentials mate!');
+          return false;
+        }
+      });
     }
     return false;
   } catch (error) {
