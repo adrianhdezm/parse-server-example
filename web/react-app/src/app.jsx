246f428669ecada15fbe9eea9b3868aff072ed88
Interlocus MacBookPro <developer@i-locus.com>
2024-08-26 18:27:02 +0900

    Add upload-form for CSV file upload

--- web/react-app/src/app.jsx
+++ web/react-app/src/app.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 import { LoginForm } from "./login-form";
 import { parseLogin } from "./parse-service";
+import { UploadForm } from "./upload-form";
 
 export const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -28,12 +29,20 @@ export const App = () => {
     setUsername(null);
   }, [setUsername, setLoggedIn]);
 
+  const handleFileUpload = (file) => {
+    console.log('File selected:', file.name);
+    // You can handle the file upload logic here, e.g., upload to a server
+  };
+ 
+
   return (
     <div>
       {loggedIn && username ? (
         <div>
           <h2>Welcome, {username}!</h2>
           <button onClick={handleLogout}>Logout</button>
+          <h1>CSV File Upload </h1>
+          <UploadForm onFileUpload={handleFileUpload} />
         </div>
       ) : (
         <LoginForm onLogin={handleLogin} />
